b8a488116ea547b71f275f32912d2c908daf3911
Alameen688 <ogundiran12@gmail.com>
2018-06-28 18:02:45 +0100

    complete api documentation

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -27,23 +27,91 @@ use Validator;
  *         description="API documentation for Oyapay fullstack challenge",
  *         @SWG\Contact(name="Ogundiran Al-Ameen", email="ogundiran12@gmail.com"),
  *     ),
- *     @SWG\Definition(
- *         definition="Admin",
- *         required={"code", "message"},
- *         @SWG\Property(
- *             property="code",
- *             type="integer",
- *             format="int32"
- *         ),
- *         @SWG\Property(
- *             property="message",
- *             type="string"
- *         )
- *     )
+ *      @SWG\Definition(
+ * 			definition="Agent",
+ * 			required={"name", "phone_number", "business_name"},
+ * 			@SWG\Property(property="id", type="string", example="2"),
+ * 			@SWG\Property(property="name", type="string", example="Sola Kayode"),
+ * 			@SWG\Property(property="phone_number", type="string", example="08134021453"),
+ * 			@SWG\Property(property="created_at", type="string", example="2018-06-28 14:26:08"),
+ * 		),
+ *      @SWG\Definition(
+ * 			definition="Admin",
+ * 			required={"name", "phone_number", "business_name"},
+ * 			@SWG\Property(property="id", type="string", example="2"),
+ * 			@SWG\Property(property="name", type="string", example="Simi"),
+ * 			@SWG\Property(property="phone_number", type="string", example="08134021453"),
+ * 			@SWG\Property(property="business_name", type="string", example="Oyapay"),
+ * 			@SWG\Property(property="created_at", type="string", example="2018-06-28 14:26:08"),
+ * 		),
+ * 
+ *       @SWG\Definition(
+ * 			definition="NewAdmin",
+ * 			required={"name", "phone_number", "password", "password_confirmation", "business_name"},
+ * 			@SWG\Property(property="name", type="string", example="Simi"),
+ * 			@SWG\Property(property="phone_number", type="string", example="08134021453"),
+ * 			@SWG\Property(property="business_name", type="string", example="Oyapay"),
+ * 			@SWG\Property(property="password", type="string", example="whatever"),
+ * 			@SWG\Property(property="password_confirmation", type="string", example="whatever"),
+ * 		),
+ * 
+ *      @SWG\Definition(
+ * 			definition="Invite",
+ * 			required={"name", "phone_number", "token", "status"},
+ * 			@SWG\Property(property="id", type="string", example="2"),
+ * 			@SWG\Property(property="name", type="string", example="Sola Kayode"),
+ * 			@SWG\Property(property="phone_number", type="string", example="08134021453"),
+ * 			@SWG\Property(property="token", type="string", example="$2y$10$uACQUstEGKVM7vlV6DvrN.NJFJqlE1nDIQ7gJUvJI8pe4Mnj49cAq"),
+ * 			@SWG\Property(property="status", type="string", example="pending"),
+ * 			@SWG\Property(property="created_at", type="string", example="2018-06-28 14:26:08"),
+ * 		),
+ * 
+ *      @SWG\Definition(
+ * 			definition="CompletedInvite",
+ * 			required={"name", "phone_number", "token", "status"},
+ * 			@SWG\Property(property="id", type="string", example="2"),
+ * 			@SWG\Property(property="name", type="string", example="Osoko Biodun"),
+ * 			@SWG\Property(property="phone_number", type="string", example="08134021453"),
+ * 			@SWG\Property(property="token", type="string", example="$2y$10$uzYilYJX1y/Ey0EkOuwS.e6UoRjlbiW3my/my5J659g5RGUUBLNju"),
+ * 			@SWG\Property(property="status", type="string", example="completed"),
+ * 			@SWG\Property(property="created_at", type="string", example="2018-06-28 14:26:08"),
+ * 		),
+ * 
+ * 
  * )
  */
 class ApiController extends Controller
 {
+    /**
+     * @SWG\Get(
+     *      path="/invites",
+     *      tags={"Invites"},
+     *      operationId="getAllInvites",
+     *      summary="Get all invites",
+     *      description="Returns information of all invites that has been sent",
+     *      
+     *      @SWG\Response(
+     *          response=200,
+     *          description="success",
+     *              @SWG\Schema(
+     *                  @SWG\Property(
+     *                      property="status",
+     *                      example="success",
+     *                  ),
+     *                  @SWG\Property(
+     *                      property="data", 
+     *                      type="array", 
+     *                      @SWG\Items(
+     *                          ref="#/definitions/Invite"
+     *                      )
+     *                  ),
+     *                  
+     *              ),
+     *       ),
+     * 
+     * )
+     *
+     */
     public function getAllInvites()
     {
         $invites = Invite::all();
@@ -53,6 +121,36 @@ class ApiController extends Controller
         return new InviteResource($result);
     }
 
+    /**
+     * @SWG\Get(
+     *      path="/invites/pending",
+     *      tags={"Invites"},
+     *      operationId="getPendingInvites",
+     *      summary="Get all pending invites",
+     *      description="Returns information of all pending invites",
+     *      
+     *      @SWG\Response(
+     *          response=200,
+     *          description="success",
+     *              @SWG\Schema(
+     *                  @SWG\Property(
+     *                      property="status",
+     *                      example="success",
+     *                  ),
+     *                  @SWG\Property(
+     *                      property="data", 
+     *                      type="array", 
+     *                      @SWG\Items(
+     *                          ref="#/definitions/Invite"
+     *                      )
+     *                  ),
+     *                  
+     *              ),
+     *       ),
+     * 
+     * )
+     *
+     */
     public function getPendingInvites()
     {
         $invites = Invite::where('status','pending')->latest()->get();
@@ -62,6 +160,37 @@ class ApiController extends Controller
         return new InviteResource($result);
     }
 
+
+    /**
+     * @SWG\Get(
+     *      path="/invites/completed",
+     *      tags={"Invites"},
+     *      operationId="getCompletedInvites",
+     *      summary="Get all completed invites",
+     *      description="Returns information of all completed invites",
+     *      
+     *      @SWG\Response(
+     *          response=200,
+     *          description="success",
+     *              @SWG\Schema(
+     *                  @SWG\Property(
+     *                      property="status",
+     *                      example="success",
+     *                  ),
+     *                  @SWG\Property(
+     *                      property="data", 
+     *                      type="array", 
+     *                      @SWG\Items(
+     *                          ref="#/definitions/CompletedInvite"
+     *                      )
+     *                  ),
+     *                  
+     *              ),
+     *       ),
+     * 
+     * )
+     *
+     */
     public function getCompletedInvites()
     {
         $invites = Invite::where('status','completed')->latest()->get();
@@ -71,6 +200,36 @@ class ApiController extends Controller
         return new InviteResource($result);
     }
 
+    /**
+     * @SWG\Get(
+     *      path="/users/admin",
+     *      tags={"Admin"},
+     *      operationId="getAdminUsers",
+     *      summary="Get all admin accounts",
+     *      description="Returns information of all existing admin accounts",
+     *      
+     *      @SWG\Response(
+     *          response=200,
+     *          description="successful operation",
+     *              @SWG\Schema(
+     *                  @SWG\Property(
+     *                      property="status",
+     *                      example="success",
+     *                  ),
+     *                  @SWG\Property(
+     *                      property="data", 
+     *                      type="array", 
+     *                      @SWG\Items(
+     *                          ref="#/definitions/Admin"
+     *                      )
+     *                  ),
+     *                  
+     *              ),
+     *       ),
+     * 
+     * )
+     *
+     */
     public function getAdminUsers()
     {
         $allAdmin = User::where('role', 'admin')->latest()->get(); 
@@ -80,6 +239,37 @@ class ApiController extends Controller
         return new UserCollection($result);
     }
 
+    /**
+     * @SWG\Get(
+     *      path="/find/admin/{phone_number}",
+     *      operationId="getAdminByPhone",
+     *      tags={"Admin"},
+     *      summary="Get admin details by phone number",
+     *      description="Gets admin details using phone number and returns the info",
+     *      @SWG\Parameter(
+     *          name="phone_number",
+     *          description="admin's phone number",
+     *          required=true,
+     *          type="string",
+     *          in="path"
+     *      ),
+     *      @SWG\Response(
+     *          response=200,
+     *          description="success",
+     *          @SWG\Schema(
+     *              @SWG\Property(
+     *                  property="status",
+     *                  example="success",
+     *              ),
+     *              @SWG\Property(
+     *                  property="data",
+     *                  ref="#/definitions/Admin"
+     *              )
+     *          )
+     *       ),
+     * )
+     *
+     */
     public function getAdminByPhone($phone)
     {
         $admin = User::where('role', 'admin')
@@ -99,6 +289,37 @@ class ApiController extends Controller
         return new UserResource($result);
     }
 
+    /**
+     * @SWG\Get(
+     *      path="/users/agent",
+     *      tags={"Agent"},
+     *      operationId="getAgentUsers",
+     *      summary="Get all agent accounts",
+     *      description="Returns information of all existing agent accounts",
+     *      
+     *      @SWG\Response(
+     *          response=200,
+     *          description="success",
+     *              @SWG\Schema(
+     *                  @SWG\Property(
+     *                      property="status",
+     *                      example="success",
+     *                  ),
+     *                  @SWG\Property(
+     *                      property="data", 
+     *                      type="array", 
+     *                      @SWG\Items(
+     *                          ref="#/definitions/Agent"
+     *                      )
+     *                  ),
+     *                  
+     *              ),
+     *       ),
+     * 
+     * )
+     *
+     */
+    
     public function getAgentUsers()
     {
         $allAgent = User::where('role', 'agent')->latest()->get(); 
@@ -108,6 +329,38 @@ class ApiController extends Controller
 
         return new UserCollection($result);
     }
+
+    /**
+     * @SWG\Get(
+     *      path="/find/agent/{phone_number}",
+     *      operationId="getAgentByPhone",
+     *      tags={"Agent"},
+     *      summary="Get agent details by phone number",
+     *      description="Gets agent details using phone number and returns the info",
+     *      @SWG\Parameter(
+     *          name="phone_number",
+     *          description="agent's phone number",
+     *          required=true,
+     *          type="string",
+     *          in="path"
+     *      ),
+     *      @SWG\Response(
+     *          response=200,
+     *          description="success",
+     *          @SWG\Schema(
+     *              @SWG\Property(
+     *                  property="status",
+     *                  example="success",
+     *              ),
+     *              @SWG\Property(
+     *                  property="data",
+     *                  ref="#/definitions/Agent",
+     *              )
+     *          )
+     *       ),
+     * )
+     *
+     */
 
     public function getAgentByPhone($phone)
     {
@@ -128,7 +381,46 @@ class ApiController extends Controller
         return new UserResource($result);
     }
 
-
+    /**
+     * Create a new admin account
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     *
+     * @SWG\Post(
+     *     path="/admin/create",
+     *     description="Registers a new user and returns details.",
+     *     operationId="createAdmin",
+     *     produces={"application/json"},
+     *     tags={"Admin"},
+     *     @SWG\Parameter(
+     *         name="body",
+     *         in="body",
+     *         description="",
+     *         @SWG\Schema(
+     *              ref="#/definitions/NewAdmin",
+     *         )
+     *      ),
+     *     @SWG\Response(
+     *         response=200,
+     *         description="success",
+     *         @SWG\Schema(
+     *              @SWG\Property(
+     *                  property="status",
+     *                  example="success",
+     *              ),
+     *              @SWG\Property(
+     *                  property="data",
+     *                  ref="#/definitions/Admin",
+     *              )
+     *          )
+     *     ),
+     *     @SWG\Response(
+     *         response=500,
+     *         description="Internal server error",
+     *     )
+     * )
+     */
     
     public function createAdmin()
     {
